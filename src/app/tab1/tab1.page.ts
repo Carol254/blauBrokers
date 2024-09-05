bf0a87b5776549eb876312ca26ecdaae914ca2c0
Caroline <wanguic49@gmail.com>
2024-09-05 12:48:21 +0300

    few changes made

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton, IonCard, IonText, IonCol, IonRow } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from "ionicons";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,IonButton],
+  imports: [IonRow, IonCol, IonText, IonCard, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,IonButton],
 })
 export class Tab1Page {
   constructor() {}
